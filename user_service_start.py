dddd6d2bb363abedfa47f95b16319fb7b9ac783a
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-05-05 14:24:40 +0800

    Changed host= in user_service_start.py

--- user_service_start.py
+++ user_service_start.py
@@ -3,7 +3,8 @@ from src.config import config
 from src.connections import get_ip
 
 
-host = get_ip()
+# host = get_ip()
+host = "0.0.0.0"
 port = config["user_service_port"]
 
 uvicorn.run("user_service:app", host=host, port=port)
