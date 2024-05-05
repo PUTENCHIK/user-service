6ffea417b947fbc2a7188f903c890295398840c5
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-05-05 14:26:11 +0800

    Changed host= in publisher_start.py

--- publisher_start.py
+++ publisher_start.py
@@ -3,7 +3,8 @@ from src.config import config
 from src.connections import get_ip
 
 
-host = get_ip()
+# host = get_ip()
+host = "0.0.0.0"
 port = config["publisher_port"]
 
 uvicorn.run("publisher:app", host=host, port=port)
